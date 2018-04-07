40d02b4f3b97c93fa9ba84b8f7064d9761d781b0
Grzegorz Surma <grzegorz.surma@icloud.com>
2018-04-07 14:00:54 +0200

    initial commit

---
+++ DiffieHellmanKeyExchange/DiffieHellmanKeyExchange/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DiffieHellmanKeyExchange
+//
+//  Created by Grzegorz Surma on 07/04/2018.
+//  Copyright © 2018 Grzegorz Surma. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
